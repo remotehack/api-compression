940decf1751a9bc8b0ad32d11880cd65a1a49fec
Ben Foxall <benfoxall@gmail.com>
2020-07-25 13:29:09 +0100

    JS hacks!

--- node/server.js
+++ node/server.js
@@ -7,7 +7,23 @@ const port = 3000;
 const content = "Hello World!";
 
 app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
-app.get("/data", (req, res) => res.send(content));
+app.get("/data", (req, res) => {
+  console.log(req.header("content-encoding"));
+
+  switch (req.header("content-encoding")) {
+    case "chars":
+      const parts = Array.from(content)
+        .map((c) => c.charCodeAt(0))
+        .join(", ");
+
+      res.send(parts);
+
+      break;
+
+    default:
+      res.send(content);
+  }
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
