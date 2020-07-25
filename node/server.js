c2f7539e453235fee6d1e747340ebb9110f8fec8
Ben Foxall <benfoxall@gmail.com>
2020-07-25 14:28:14 +0100

    Emoji

--- node/server.js
+++ node/server.js
@@ -7,11 +7,14 @@ const port = 3000;
 const content = "Hello World!";
 
 app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+const encoder = new TextEncoder();
+
 app.get("/data", (req, res) => {
   console.log(req.header("content-encoding"));
 
   switch (req.header("content-encoding")) {
-    case "chars":
+    case "chars": {
       const parts = Array.from(content)
         .map((c) => c.charCodeAt(0))
         .join(", ");
@@ -19,7 +22,21 @@ app.get("/data", (req, res) => {
       res.send(parts);
 
       break;
+    }
 
+    case "emoji": {
+      const parts = Array.from(encoder.encode(content))
+        .map((n) =>
+          Array.from(n.toString(2))
+            .map((c) => (c === "1" ? one : zero))
+            .join("")
+        )
+        .join("");
+
+      res.send(parts);
+
+      break;
+    }
     default:
       res.send(content);
   }
