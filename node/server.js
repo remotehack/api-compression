6c9a96e8e04d8ee07eccb8ee74212da43f68e9fc
Ben Foxall <benfoxall@gmail.com>
2020-07-25 12:26:33 +0100

    Stub out html page

--- node/server.js
+++ node/server.js
@@ -1,9 +1,13 @@
+const { join } = require("path");
 const express = require("express");
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const content = "Hello World!";
+
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+app.get("/data", (req, res) => res.send(content));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
